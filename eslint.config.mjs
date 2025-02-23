a90765a13adf9622adce3ec60d59da06411ea8c8
bogdanpikus <pikusbgdan@gmail.com>
2025-02-23 19:10:19 +0200

    style: отформатирован код с помощью Prettier chore: настройка ESLint и Prettier fix: исправлены стилистические ошибки в коде refactor: обновлены правила линтера и форматирование

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      prettier: pluginPrettier, // Подключаем плагин Prettier
+    },
+    rules: {
+      "prettier/prettier": "error", // Ошибки форматирования станут ошибками ESLint
+    },
+  },
+];
