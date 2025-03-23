296116c1950c5010561a100121c26c76baade9f1
bogdanpikus <pikusbgdan@gmail.com>
2025-03-23 18:33:20 +0200

    fear: добавил кнопку погоды, подшаманил в header, выронял компоненты.,добавил стиль их отображения. style: отредактировал часть css. chore:добавлены фвйлы для проектування react компонентов.

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('weather_id').onclick = myFunction;
+
+function myFunction() {
+
+
+ document.getElementById('weather_block_div').style.visibility = 'hidden';
+};
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import '../index.css';
+
 export default function Home() {
     return <h1>Привет, Next.js!</h1>;
 }
