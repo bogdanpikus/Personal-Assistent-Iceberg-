395e4a72c5d53f93f8c5d18c15e56e85360e003e
bogdanpikus <pikusbgdan@gmail.com>
2025-03-27 20:08:00 +0200

    feat: доработал кнопки и написал небольшой код для погоды и календаря

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-document.getElementById('weather_id').onclick = myFunction;
+document.getElementById('weather_id_button').onclick = closeWeather;
+document.getElementById('calendar_id_button').onclick = closeCalendar;
 
-function myFunction() {
+function closeWeather() {
+    //const block = document.getElementById('weather_block_div'); // замените на ваш id блока
+    //block.style.display = (block.style.display === 'none' || block.style.display === '') ? 'inline-block' : 'none';
 
-
- document.getElementById('weather_block_div').style.visibility = 'hidden';
+    const button = document.getElementById('weather_id_button');
+    const block = document.getElementById('weather_block_div');
+    button.addEventListener('click', function () {
+        block.style.display = (block.style.display === 'none' || block.style.display === '') ? 'inline-block' : 'none';
+    });
 };
+
+function closeCalendar() {
+    //const block = document.getElementById('calendar_block_div');
+    //block.style.display = (block.style.display === 'none' || block.style.display === '') ? 'inline-block' : 'none';
+
+    const button = document.getElementById('calendar_id_button');
+    const block = document.getElementById('calendar_block_div');
+    button.addEventListener('click', function () {
+        block.style.display = (block.style.display === 'none' || block.style.display === '') ? 'inline-block' : 'none';
+    });
+}; 
