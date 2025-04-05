41aff62b911dc62f161110775757bec07a77a5df
bogdanpikus <pikusbgdan@gmail.com>
2025-04-06 01:05:55 +0300

    feat: улучшил отображение Даты

--- components/Date.js
+++ components/Date.js
@@ -8,7 +8,24 @@ let options = {
 
 let date = nowDate.toLocaleString("uk-UA", options);
 
-document.getElementById("date_block_div").innerHTML =
+document.getElementById("dateBlock").innerHTML =
     `<p>${date}</p>`;
 
-console.log(nowDate.toLocaleString());
+let season = nowDate.getMonth(); // 3
+let Img_SeasonChange = document.getElementById('date_block_div');
+//если мес€ц от 0 до 3 => ебашим задний фон такой, если от 4 до 7 - такой и тд
+// скорее всего оптимизирую с помощью массива либо задействую switch
+function ChangeImageDate(season) {
+    if ((season >= 0 && season <= 2) || season == 11) {
+        Img_SeasonChange.style.background = 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu2d5yiu73B4N7qqOCGM9Sfh2vGEl8VOWeSw&s) center';
+    } else if (season > 2 && season <= 4) {
+        Img_SeasonChange.style.background = 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs5y_xClyj0Rc0jtOc-jjGYck7WnvxsQi24Q&s) center';
+    } else if (season > 4 && season <= 7){
+        Img_SeasonChange.style.background = 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMXEhaKhTBArzNOeNrRHuVM0GBgpQrHhcsla0pOPX9fE3VLcKz6KsTQJGrynRmGhWzTsI&usqp=CAU) center';
+    } else if (season > 7 && season <= 10) {
+        Img_SeasonChange.style.background = 'url(https://i0.wp.com/dotsandbrackets.com/wp-content/uploads/2023/09/autumn.jpg?resize=1400%2C800&ssl=1) center';
+    }
+};
+ChangeImageDate(season);
+
+console.log(season);
--- components/Progress.js
+++ components/Progress.js
@@ -1,26 +1,64 @@
+
 let div_storage = document.getElementById("input_progress_storage");
 let number = 0;
+/////////////////////////////////////////////Создается база данных
+let bd;
+let openRequest = indexedDB.open("Tasks");
+openRequest.onerror = function () {
+    console.error('Error', openRequest.error);
+};
+openRequest.onupgradeneeded = function () {
+    let Tasks = openRequest.result;
+    let name = `TasksNames`;
+    if (!Tasks.objectStoreNames.contains(name)) {
+        Tasks.createObjectStore(name, { keyPath: 'id' });
+    }
+};
+openRequest.onsuccess = function () {
+    bd = openRequest.result;
+  //  alert(bd);
+};
+////////////////////////////////////////////////////////
 
 function openModal() {
     document.getElementById('modal_overlay').style.display = 'flex';
-}
+};
 
 function closeModal() {
     document.getElementById('modal_overlay').style.display = 'none';
-}
+};
 
 document.getElementById('progress_adding_button').addEventListener('click', openModal);
 document.getElementById('cancel_note_button').addEventListener('click', closeModal);
 document.getElementById('save_note_button').addEventListener('click', () => {
     const value = document.getElementById('note_input').value.trim();
     if (value) {
-        // Добавь задачу в список и сохрани в indexedDB, если хочешь
+        ////////////////////////////////////////////// Добавь задачу в список и сохрани в indexedDB
+        let transaction = bd.transaction("TasksNames");
+        let tasks = transaction.objectStore("TasksNames");
+        let task = {
+            id: 'js',
+            price: 10,
+            created: new Date()
+        }
+
+        let request = tasks.add(task);
+        request.onsuccess = function () {
+            console.log("Task added", request.result);
+        };
+        request.onerror = function () {
+            console.log('Error', request.error);
+        }
+        ///////////////////////////////////////////////
+
+        document.getElementById("done_progress_button").style.display = 'inline-block';
         closeModal();
     } else {
         alert("Пожалуйста, введите текст задачи");
     }
 });
 
+console.log(openRequest);
 
 /*document.getElementById('progress_adding_button').addEventListener('click', () => {
     const form = document.getElementById('note_form');
