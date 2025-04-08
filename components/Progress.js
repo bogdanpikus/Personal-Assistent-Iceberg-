0d2efd2a98bee70396749309f0c395cb134778ff
bogdanpikus <pikusbgdan@gmail.com>
2025-04-08 22:21:07 +0300

    feat: update Progress.js add indexedDB

--- components/Progress.js
+++ components/Progress.js
@@ -1,23 +1,31 @@
 
 let div_storage = document.getElementById("input_progress_storage");
 let number = 0;
+let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+let IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
 /////////////////////////////////////////////Создается база данных
-let bd;
-let openRequest = indexedDB.open("Tasks");
-openRequest.onerror = function () {
-    console.error('Error', openRequest.error);
-};
-openRequest.onupgradeneeded = function () {
-    let Tasks = openRequest.result;
-    let name = `TasksNames`;
-    if (!Tasks.objectStoreNames.contains(name)) {
-        Tasks.createObjectStore(name, { keyPath: 'id' });
-    }
-};
+let openRequest = indexedDB.open("Tasks"); //Щоб почати працювати з IndexedDB, нам спочатку потрібно відкрити (підключитися до) бази даних.
+let db;    
+
 openRequest.onsuccess = function () {
-    bd = openRequest.result;
-  //  alert(bd);
+    db = openRequest.result;
+}
+
+// створити/оновити базу даних без перевірки версій
+openRequest.onupgradeneeded = function () {
+    // спрацьовує, якщо на клієнті немає бази даних
+    // ...виконати ініціалізацію...
+    db = openRequest.result;
+
+    db.createObjectStore("TaskDB", { keyPath: "id" });
+
+   
 };
+
+openRequest.onerror = function () { //ловим ошибки
+    console.error("Error", openRequest.error);
+};
+
 ////////////////////////////////////////////////////////
 
 function openModal() {
@@ -32,52 +40,40 @@ document.getElementById('progress_adding_button').addEventListener('click', open
 document.getElementById('cancel_note_button').addEventListener('click', closeModal);
 document.getElementById('save_note_button').addEventListener('click', () => {
     const value = document.getElementById('note_input').value.trim();
+    let nowDateZip = Date.now();
+    let nowDateUnZip = new Date(nowDateZip);
+  //  let day_month_year = `${nowDateUnZip.getDay()}:${nowDateUnZip.getMonth()}:${nowDateUnZip.getFullYear()}:${nowDateUnZip.getSeconds()}:${nowDateUnZip.getMinutes()}`;
+    let day_month_year = nowDateUnZip.toLocaleString("ua-UA");
     if (value) {
-        ////////////////////////////////////////////// Добавь задачу в список и сохрани в indexedDB
-        let transaction = bd.transaction("TasksNames");
-        let tasks = transaction.objectStore("TasksNames");
-        let task = {
-            id: 'js',
-            price: 10,
-            created: new Date()
-        }
+        ////////////////////////////////////////////// Добавить задачу в список и сохрани в indexedDB
+        let Store = db.transaction('TaskDB', 'readwrite').objectStore('TaskDB'); // (1)
 
-        let request = tasks.add(task);
-        request.onsuccess = function () {
-            console.log("Task added", request.result);
+        // отримати сховище об’єктів для роботи з ним
+       // let Store = transaction.objectStore('TaskDB'); // (2)
+
+        let task = {
+            id: day_month_year, // простой способ создать уникальный id
+            text: value
+        };  
+
+        let request = Store.add(task); // (3)
+
+        request.onsuccess = function () { // (4)
+            console.log("Task add to storage", request.result);
         };
+
         request.onerror = function () {
-            console.log('Error', request.error);
-        }
-        ///////////////////////////////////////////////
+            console.log("Fatal Error", request.error);
+        };
+            ///////////////////////////////////////////////
+
 
         document.getElementById("done_progress_button").style.display = 'inline-block';
         closeModal();
     } else {
-        alert("Пожалуйста, введите текст задачи");
+        alert("Please write some text");
     }
 });
 
-console.log(openRequest);
-
-/*document.getElementById('progress_adding_button').addEventListener('click', () => {
-    const form = document.getElementById('note_form');
-    form.style.display = (form.style.display === 'none') ? 'block' : 'none';
-});
-
-document.getElementById('progress_adding_button').addEventListener('click', () => {
-    number++;
-
-    div_storage.innerHTML = '';
-    let div_note = document.createElement("div"); //создаем элемент div
-    div_note.className = `div_task_${number}`;
-    let checkbox = document.createElement("input");
-    let button = document.createElement("button");
-    div_storage.appendChild(div_note); //говорим что div_note это подэлемент div_storage
-    div_note.appendChild(checkbox);
-    div_note.appendChild(button);
-
-
-    console.log(div_note);
-    console.log(div_storage);
-}); */
+console.log(db);
+console.log(openRequest.onupgradeneeded);
