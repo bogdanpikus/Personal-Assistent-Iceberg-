5a7307738d5184996534645ab6f197814ab51b9f
Exployer <pikusbgdan@gmail.com>
2025-05-28 11:20:33 +0300

    Revert "feat:"
    This reverts commit 9eaa385cc58fdb8787aa7d6c58c62ca860755366.

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ document.getElementById('weather_id_button').addEventListener('click', function 
     weather_block.style.display = (weather_block.style.display === 'none' || weather_block.style.display === '') ? 'inline-block' : 'none';
 });
 
+const calendar_block = document.getElementById('calendar_block_div');
+document.getElementById('calendar_id_button').addEventListener('click', function () {
+    calendar_block.style.display = (calendar_block.style.display === 'none' || calendar_block.style.display === '') ? 'inline-block' : 'none';
+});
+
 const time_block = document.getElementById('time_block_div');
 document.getElementById('time_id_button').addEventListener('click', function () {
     time_block.style.display = (time_block.style.display === 'none' || time_block.style.display === '') ? 'inline-block' : 'none';
